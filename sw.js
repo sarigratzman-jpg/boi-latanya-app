8d83b52de14f40f9d2ab5a3f3d10397f01b8778a
sarigratzman-jpg <sarigratzman@gmail.com>
2026-09-22 16:56:29 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request).catch(() => new Response('', {status: 200})));
+});
